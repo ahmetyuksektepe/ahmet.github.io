cc70194ff8a0c126389c7b9f3e0798508903a4a4
Ahmet <ahmetf_y@outlook.com>
2026-07-04 13:47:25 +0300

    domain added

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,8 @@
 import { withContentCollections } from "@content-collections/next";
 
-const repoName = "ahmet.github.io";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? `/${repoName}` : "",
-  assetPrefix: process.env.NODE_ENV === "production" ? `/${repoName}/` : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
